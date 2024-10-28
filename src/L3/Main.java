5031294c087ac41992bb282e3e522c1a36136466
Jack <jackn25@nycstudents.net>
2024-10-28 13:17:50 -0400

    finished l3

---
+++ src/L3/Main.java
@@ -0,0 +1,17 @@
+package L3;
+
+public class Main {
+    public static void main(String[] args) {
+        int p = 5;
+        int q = 0;
+
+        if (p > 0 && (p / q) == 2)
+        {
+            System.out.println("it’s true :)");
+        }
+        else
+        {
+            System.out.println("it’s false :(");
+        }
+    }
+}
